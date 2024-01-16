640df68107bbff2e7cffc6e2d7a805717377984e
willians2022 <willians_zno@hotmail.com>
2024-01-16 08:24:48 -0300

    criação do projeto jogo da memoria

---
+++ src/scripts/engine.js
@@ -0,0 +1,27 @@
+const emojis = [
+   "🚗",
+   "🚗",
+   "🍉",
+   "🍉",
+   "🍓",
+   "🍓",
+   "👮‍♂️",
+   "👮‍♂️",
+   "🎅",
+   "🎅",
+   "😍",
+   "😍",
+   "🤑",
+   "🤑",
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(()=> (Math.random() > 0.5 ? 2: -1)); 
+
+for(let i=0; i < emojis.length; i++)
+{
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
