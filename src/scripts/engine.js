651d739284c4a877f8e4276df0f771e38c935cde
willians2022 <willians_zno@hotmail.com>
2024-01-16 19:56:26 -0300

    finalização do jogo da memoria

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,27 +1,56 @@
 const emojis = [
-   "🚗",
-   "🚗",
-   "🍉",
-   "🍉",
-   "🍓",
-   "🍓",
-   "👮‍♂️",
-   "👮‍♂️",
-   "🎅",
-   "🎅",
-   "😍",
-   "😍",
-   "🤑",
-   "🤑",
+    "🚗",
+    "🚗",
+    "🍉",
+    "🍉",
+    "🍓",
+    "🍓",
+    "👮‍♂️",
+    "👮‍♂️",
+    "🎅",
+    "🎅",
+    "😍",
+    "😍",
+    "🤑",
+    "🤑",
 ];
 let openCards = [];
 
-let shuffleEmojis = emojis.sort(()=> (Math.random() > 0.5 ? 2: -1)); 
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
-for(let i=0; i < emojis.length; i++)
-{
+for (let i = 0; i < emojis.length; i++) {
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
     document.querySelector(".game").appendChild(box);
+}
+
+function handleClick() {
+    if (openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if (openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+
+    console.log(openCards);
+}
+
+function checkMatch() {
+    if (openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    openCards = [];
+
+    if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+        alert("Você venceu !");
+    }
 }
